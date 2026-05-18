5dad540b88197636b5893c1a4f339a14e0659ffa
Ubuntu <ubuntu@ip-172-31-16-40.ec2.internal>
2026-05-18 22:23:57 +0000

    fix: make SMTP server optional for cloud deployment

--- src/index.js
+++ src/index.js
@@ -5,30 +5,33 @@ const store = require('./store');
 
 const PORT = process.env.PORT || 3000;
 const SMTP_PORT = process.env.SMTP_PORT || 25;
+const SMTP_ENABLED = process.env.SMTP_ENABLED !== 'false';
 
 const expressApp = createWebServer();
-const smtpServer = createSMTPServer();
-
-smtpServer.on('error', (err) => {
-  console.error('SMTP server error:', err.message);
-});
 
 const webServer = expressApp.listen(PORT, () => {
   console.log(`Web server running on http://localhost:${PORT}`);
 });
 
-smtpServer.listen(SMTP_PORT, '0.0.0.0', () => {
-  console.log(`SMTP server listening on port ${SMTP_PORT}`);
-});
+if (SMTP_ENABLED) {
+  const smtpServer = createSMTPServer();
+  
+  smtpServer.on('error', (err) => {
+    console.error('SMTP server error:', err.message);
+  });
+
+  smtpServer.listen(SMTP_PORT, '0.0.0.0', () => {
+    console.log(`SMTP server listening on port ${SMTP_PORT}`);
+  });
+} else {
+  console.log('SMTP server disabled (set SMTP_ENABLED=true to enable)');
+}
 
 store.startCleanup();
 
 function gracefulShutdown(signal) {
   console.log(`\n${signal} received, shutting down...`);
   store.stopCleanup();
-  smtpServer.close(() => {
-    console.log('SMTP server closed');
-  });
   webServer.close(() => {
     console.log('Web server closed');
     process.exit(0);
